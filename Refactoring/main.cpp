ec75623e250fe9e677853a01a3ce5575416dfb26
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2024-03-18 18:40:40 +0000

    Update main.cpp

--- Refactoring/main.cpp
+++ Refactoring/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "shape.h"
-#include "transform.h"
 
 int main() {
-
     std::setlocale(LC_ALL, "RU");
-  
+
     Shape square(Shape::SQUARE, { {0, 0, 0}, {1, 0, 0}, {1, 1, 0}, {0, 1, 0} });
 
     std::cout << "Before transformation:\n";
@@ -18,15 +16,13 @@ int main() {
     std::cout << "Volume: " << square.getVolume() << "\n";
     std::cout << "Square: " << square.getSquare() << "\n\n";
 
-   
-    Transform transform(square);
-    transform.shift(1, 1, 0);
-    transform.scaleX(2);
-    transform.scaleY(2);
-    transform.scaleZ(2);
-    transform.scale(2);
+    // Преобразования
+    square.shift(1, 1, 0);
+    square.scaleX(2);
+    square.scaleY(2);
+    square.scaleZ(2);
+    square.scale(2);
 
-   
     std::cout << "After transformation:\n";
     std::cout << "Type: Square\n";
     std::cout << "Points:\n";
