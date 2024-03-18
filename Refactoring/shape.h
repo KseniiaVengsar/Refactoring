7b77b9c72ce995ce071a5f6b40c3b6e43ac2badb
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2024-03-18 18:28:09 +0000

    Update shape.h

--- Refactoring/shape.h
+++ Refactoring/shape.h
@@ -14,6 +14,8 @@ public:
         const Point& p5, const Point& p6, const Point& p7, const Point& p8);
     Shape(Type t, const Point& center, double R, double H);
 
+    virtual ~Shape() = default;
+
     Type getType() const { return type; }
     int getNumPoints() const { return points.size(); }
     const Point& operator[](int i) const { return points[i]; }
@@ -21,12 +23,38 @@ public:
     double getVolume() const { return volume; }
     double getSquare() const { return square; }
 
-private:
+    virtual void shift(int m, int n, int k);
+    virtual void scaleX(int a);
+    virtual void scaleY(int d);
+    virtual void scaleZ(int e);
+    virtual void scale(int s);
+
+protected:
     Type type;
     std::vector<Point> points;
     double volume;
     double square;
 
     void calculateVolumeAndSquare();
+};
+
+class Line : public Shape {
+public:
+    Line(const Point& p1, const Point& p2);
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(const Point& p1, const Point& p2, const Point& p3, const Point& p4);
+};
+
+class Circle : public Shape {
+public:
+    Circle(const Point& center, double R);
+};
+
+class Cylinder : public Shape {
+public:
+    Cylinder(const Point& center, double R, double H);
 };
 
