3f2f61fc124ce8b2ea9827aefd634c33243cbeb9
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2024-03-18 18:28:37 +0000

    Update shape.h

--- Refactoring/shape.h
+++ Refactoring/shape.h
@@ -1,60 +1,82 @@
-#pragma once
-#include <vector>
+#define _USE_MATH_DEFINES
+#include "shape.h"
+#include <cmath>
 
-struct Point {
-    int x, y, z;
-};
+Shape::Shape(Type t, const std::vector<Point>& pts) : type(t), points(pts) {
+    calculateVolumeAndSquare();
+}
 
-class Shape {
-public:
-    enum Type { LINE, SQUARE, CUBE, CIRCLE, CYLINDER };
+Shape::Shape(Type t, const Point& p1, const Point& p2, const Point& p3, const Point& p4,
+    const Point& p5, const Point& p6, const Point& p7, const Point& p8) : type(t) {
+    points.push_back(p1);
+    points.push_back(p2);
+    points.push_back(p3);
+    points.push_back(p4);
+    points.push_back(p5);
+    points.push_back(p6);
+    points.push_back(p7);
+    points.push_back(p8);
+    calculateVolumeAndSquare();
+}
 
-    Shape(Type t, const std::vector<Point>& points);
-    Shape(Type t, const Point& p1, const Point& p2, const Point& p3, const Point& p4,
-        const Point& p5, const Point& p6, const Point& p7, const Point& p8);
-    Shape(Type t, const Point& center, double R, double H);
+Shape::Shape(Type t, const Point& center, double R, double H) : type(t) {
+    points.push_back(center);
+    if (t == CIRCLE || t == CYLINDER) {
+        points[0].z = 0;
+    }
+    if (t == CYLINDER) {
+        Point top_center = { center.x, center.y, H };
+        points.push_back(top_center);
+        Point bottom_center = { center.x, center.y, 0 };
+        points.push_back(bottom_center);
+    }
+    calculateVolumeAndSquare();
+}
 
-    virtual ~Shape() = default;
+void Shape::shift(int m, int n, int k) {
+    for (int i = 0; i < points.size(); ++i) {
+        points[i].x += m;
+        points[i].y += n;
+        points[i].z += k;
+    }
+}
 
-    Type getType() const { return type; }
-    int getNumPoints() const { return points.size(); }
-    const Point& operator[](int i) const { return points[i]; }
-    Point& operator[](int i) { return points[i]; }
-    double getVolume() const { return volume; }
-    double getSquare() const { return square; }
+void Shape::scaleX(int a) {
+    for (int i = 0; i < points.size(); ++i) {
+        points[i].x *= a;
+    }
+}
 
-    virtual void shift(int m, int n, int k);
-    virtual void scaleX(int a);
-    virtual void scaleY(int d);
-    virtual void scaleZ(int e);
-    virtual void scale(int s);
+void Shape::scaleY(int d) {
+    for (int i = 0; i < points.size(); ++i) {
+        points[i].y *= d;
+    }
+}
 
-protected:
-    Type type;
-    std::vector<Point> points;
-    double volume;
-    double square;
+void Shape::scaleZ(int e) {
+    for (int i = 0; i < points.size(); ++i) {
+        points[i].z *= e;
+    }
+}
 
-    void calculateVolumeAndSquare();
-};
+void Shape::scale(int s) {
+    for (int i = 0; i < points.size(); ++i) {
+        points[i].x /= s;
+        points[i].y /= s;
+        points[i].z /= s;
+    }
+}
 
-class Line : public Shape {
-public:
-    Line(const Point& p1, const Point& p2);
-};
-
-class Rectangle : public Shape {
-public:
-    Rectangle(const Point& p1, const Point& p2, const Point& p3, const Point& p4);
-};
-
-class Circle : public Shape {
-public:
-    Circle(const Point& center, double R);
-};
-
-class Cylinder : public Shape {
-public:
-    Cylinder(const Point& center, double R, double H);
-};
-
+void Shape::calculateVolumeAndSquare() {
+    if (type == LINE || type == SQUARE) {
+        volume = 0;
+    }
+    else {
+        
+        double a = abs(points[0].x - points[1].x);
+        double b = abs(points[0].y - points[1].y);
+        double c = abs(points[0].z - points[1].z);
+        square = 2 * (a * b + a * c + b * c);
+        volume = a * b * c;
+    }
+}
